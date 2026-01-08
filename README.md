1706f324c5ef9de2e867a8a14e9373865e9ce1ff
nogengang <nogengang@gmail.com>
2026-01-08 10:15:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# capturedata_08jan26
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/nogengang/capturedata_08jan26)
---
+++ script.js
@@ -0,0 +1,8 @@
+const submitButton = document.querySelector("#submitButton");
+console.log(submitButton);
+
+const inputText = document.querySelector("#inputText");
+console.log(inputText);
+
+const inputNumber = document.querySelector("#inputNumber");
+console.log(inputNumber);
